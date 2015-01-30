caa8395702370d67c139e5dcfbde46da5b810dc6
j-panzer <panzer@sub.uni-goettingen.de>
2015-01-30 14:36:42 +0100

    Update start.rb

--- start.rb
+++ start.rb
@@ -1,11 +1,11 @@
 #!/usr/bin/env ruby
 
 #`cd .`
-`fig -f /home/jenkins/edfu/fig.yml build`
-`fig -d -f /home/jenkins/edfu/fig.yml up`
-`fig -f /home/jenkins/edfu/fig.yml run web  rake db:drop db:create db:migrate`
+`sudo fig -f /home/jenkins/edfu/fig.yml build`
+`sudo fig -d -f /home/jenkins/edfu/fig.yml up`
+`sudo fig -f /home/jenkins/edfu/fig.yml run web  rake db:drop db:create db:migrate`
 
-container = `Docker ps`
+container = `sudo Docker ps`
 container.lines { |container|
 
   if container.include? "web"
