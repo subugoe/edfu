105bae059785a74deb83af9649abf3f4f9cfff0e
Joerg Panzer <panzer@sub.uni-goettingen.de>
2015-02-03 16:37:36 +0100

    modify start.rb and add restart.rb to differentiate between start (for the first time) and restart the containers later

--- restart.rb
+++ restart.rb
@@ -1,10 +1,17 @@
 #!/usr/bin/env ruby
-#
-# use this script if you start the containers for the first time; then use restart.rb
-
 
 Dir.chdir("/home/jenkins/edfu/")
 
+
+puts "\nstop web container"
+container = `docker ps`
+container.lines { |container|
+
+  if container.include? "web"
+    container_id = container.split[0]
+    `docker stop #{container_id}`
+  end
+}
 
 if File.exist?("temp/pids/server.pid")
   `rm tmp/pids/server.pid`
@@ -13,10 +20,9 @@ end
 puts "\nfig build"
 `fig build`
 
-puts "\nfig up -d"
-`fig up -d`
+puts "\nfig restart -d"
+`fig restart -d`
 
 puts "\nfig run  web  rake db:drop db:create db:migrate create_default_user"
 `fig run  web  rake db:drop db:create db:migrate create_default_user`
-
 
