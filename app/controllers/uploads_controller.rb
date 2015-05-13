a8678d713103e7480c87680acff7c3a8d2a8e1da
Joerg Panzer <panzer@sub.uni-goettingen.de>
2015-05-13 09:49:00 +0200

    set MAX_BATCH_SIZE to 500

--- app/controllers/uploads_controller.rb
+++ app/controllers/uploads_controller.rb
@@ -16,14 +16,17 @@ class UploadsController < ApplicationController
   # todo add email for notification
   # todo add worker queue/thread for async processing
 
-  SOLR_DOMAIN = ENV['EDFU_SOLR_1_PORT_8983_TCP_ADDR']
-  SOLR_PORT   = ENV['SOLR_PORT_8983_TCP_PORT']
-  #SOLR_DOMAIN = "127.0.0.1"
-  #SOLR_PORT   = "8983"
+  #SOLR_DOMAIN = ENV['EDFU_SOLR_1_PORT_8983_TCP_ADDR']
+  #SOLR_PORT   = ENV['SOLR_PORT_8983_TCP_PORT']
+  SOLR_DOMAIN = "127.0.0.1"
+  SOLR_PORT   = "8983"
 
 
   SOLR_CONN   = RSolr.connect :url => "http://#{SOLR_DOMAIN}:#{SOLR_PORT}/solr/collection1"
-  #SOLR_CONN = RSolr.connect :url => "http://localhost:8983/solr/collection1"
+
+
+  MAX_BATCH_SIZE = 500
+
 
   # GET /uploads/new
   def new
