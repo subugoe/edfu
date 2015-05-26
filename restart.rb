307eed419d7cf01c7b987d0ba78c52473d8ea264
Joerg Panzer <panzer@sub.uni-goettingen.de>
2015-05-26 23:53:09 +0200

    no longer remove everything on restart

--- restart.rb
+++ restart.rb
@@ -8,11 +8,8 @@ if File.exist?("temp/pids/server.pid")
   `rm tmp/pids/server.pid`
 end
 
-puts "\ndocker-compose build"
-`docker-compose build`
+puts "\ndocker-compose build web"
+`docker-compose build web`
 
 puts "\ndocker-compose start web"
-`docker-compose start web`
-
-puts "\ndocker-compose run  web  rake db:drop db:create db:migrate create_default_user RAILS_ENV='production'"
-`docker-compose run  web  rake db:drop db:create db:migrate create_default_user  RAILS_ENV='production' `
+`docker-compose up`
